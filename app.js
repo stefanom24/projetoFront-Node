7d3f67960673f422563c573b3780e399f38cb65b
stefanom24 <stefanom24@gmail.com>
2024-04-01 10:25:57 -0300

    Atualizado

--- app.js
+++ app.js
@@ -14,9 +14,34 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Roteando pagina linhas
+// Roteando pagina cadastro de trens
+app.get('/trens', (req, res) => {
+    res.render('trens');
+});
+
+// Roteando pagina cadastro de linhas
 app.get('/linhas', (req, res) => {
     res.render('linhas');
+});
+
+// Roteando pagina cadastro de estações
+app.get('/estacoes', (req, res) => {
+    res.render('estacoes');
+});
+
+// Roteando pagina ocorrencias de trens
+app.get('/ocoTrens', (req, res) => {
+    res.render('');
+});
+
+// Roteando pagina ocorrencias de linhas
+app.get('/ocoLinhas', (req, res) => {
+    res.render('');
+});
+
+// Roteando pagina ocorrencias de estações
+app.get('/ocoEstacoes', (req, res) => {
+    res.render('');
 });
 
 // Recebendo dados do Login
